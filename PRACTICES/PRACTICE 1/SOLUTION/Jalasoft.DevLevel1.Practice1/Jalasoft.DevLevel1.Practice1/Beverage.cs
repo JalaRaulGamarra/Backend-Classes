3f4df2da0fbc35d48f791d407537c6f56029aed9
raul <brga.im@gmail.com>
2022-12-16 18:15:44 -0400

    adding practices folder

---
+++ PRACTICES/PRACTICE 1/SOLUTION/Jalasoft.DevLevel1.Practice1/Jalasoft.DevLevel1.Practice1/Beverage.cs
@@ -0,0 +1,62 @@
+﻿namespace Jalasoft.DevLevel1.Practice1
+{
+    public class Beverage
+    {
+        private double winePrice = 9;
+        private double beerPrice = 7.5;
+        private double sodaPrice = 6.5;
+        public BeveragesMenu Name { get; set; }
+        public byte BeverageCount { get; set; }
+        public double TotalPrice { get; set; }
+
+        public Beverage(BeveragesMenu name, byte beverageCount)
+        {
+            Name = name;
+            BeverageCount = beverageCount;
+            TotalPrice = GetBeveragePrice(name) * beverageCount;
+        }
+
+        public double WinePrice
+        {
+            get
+            {
+                return winePrice;
+            }
+        }
+        public double BeerPrice
+        {
+            get
+            {
+                return beerPrice;
+            }
+        }
+        public double SodaPrice
+        {
+            get
+            {
+                return sodaPrice;
+            }
+        }
+
+        private double GetBeveragePrice(BeveragesMenu name)
+        {
+            double price = 0;
+            switch (name)
+            {
+                case BeveragesMenu.Wine:
+                    price = WinePrice;
+                    break;
+                case BeveragesMenu.Beer:
+                    price = BeerPrice;
+                    break;
+                case BeveragesMenu.Soda:
+                    price = SodaPrice;
+                    break;
+                default:
+                    price = 0;
+                    break;
+            }
+            return price;
+        }
+    }
+}
---
+++ PRACTICES/PRACTICE 1/SOLUTION/Jalasoft.DevLevel1.Practice1/Jalasoft.DevLevel1.Practice1/DishesMenu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jalasoft.DevLevel1.Practice1
+{
+    internal enum DishesMenu
+    {
+        Spaghetti = 1,
+        Lasagna = 2,
+        Pizza = 3,
+        Calzone = 4
+    }
+}
