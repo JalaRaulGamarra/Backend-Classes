9e4b4990d82b01c75c54d8c092f4cc8e7aae9702
Emmanuel Mercado <Emmanuel.Mercado@jala.university>
2022-12-19 16:43:12 -0400

    Create class Weapon

---
+++ Jalasoft.Devlvl1.Practica2/Target.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jalasoft.Devlvl1.Practica2
+{
+    internal class Target
+    {
+    }
+}
