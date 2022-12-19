8bbed9b575f51581aa8fbb55ce6955eafea7af6d
Jonathan Baldelomar <Jonathan.Montecinos@jala.university>
2022-12-19 17:15:33 -0400

    Adding Homework

---
+++ PRACTICES/JalasoftDevLevel1SegundaPracticaJonathanBaldelomar/Shotgun.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JalasoftDevLevel1SegundaPractica
+{
+    internal class Shotgun : Weapon
+    {
+        public bool Lantern { get; set; }
+        public Shotgun()
+        {
+            this.Brand = "Winchester";
+            this.Name = "Old betsy";
+            this.MagazineSize = 2;
+            this.Color = "brown";
+            this.Range = 5;
+            this.CurrentBullets = 2;
+            this.Weight = 13;
+            this.Lantern = true;
+        }
+    }
+}
---
+++ PRACTICES/JalasoftDevLevel1SegundaPracticaJonathanBaldelomar/Weapon.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JalasoftDevLevel1SegundaPractica
+{
+    internal class Weapon
+    {
+        //Following information
+        public string Brand { get; set; }
+        public string Name { get; set; }
+        public int MagazineSize { get; set; }
+        public string Color { get; set; }
+        public int Range { get; set; }
+        public int CurrentBullets { get; set; }
+        public int Weight { get; set; }
+
+
+        public void Shoot(int targetDistance)
+        {
+            if(CurrentBullets > 0)
+            {
+                CurrentBullets--;
+                if (this.Range >= targetDistance) Console.WriteLine("Shot done");
+                else Console.WriteLine("You missed the shot");
+            }
+            else Console.WriteLine("You don't have enough bullets to shoot");
+        }
+        public void FullReload()
+        {
+            this.CurrentBullets = MagazineSize;
+            Console.WriteLine("Full reload done");
+        }
+        public void CustomReload(int bullets)
+        {
+            if (this.MagazineSize == this.CurrentBullets) Console.WriteLine("The gun is full");
+            else
+            {
+                if (bullets <= (this.MagazineSize - this.CurrentBullets))
+                {
+                    this.CurrentBullets += bullets;
+                    Console.WriteLine("Reload done");
+                }
+                else Console.WriteLine("You can't reload so many bullets");
+            }
+        }
+    }
+}
