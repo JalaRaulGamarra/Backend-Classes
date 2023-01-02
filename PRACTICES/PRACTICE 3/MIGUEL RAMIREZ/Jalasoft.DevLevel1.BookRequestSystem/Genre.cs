57b54aed5333dced45d9e8370ebf8e862d37261c
Miguel Ramirez <Miguel.Ramirez@jala.university>
2023-01-02 17:15:50 -0400

    Add Practice 3

---
+++ PRACTICES/PRACTICE 3/MIGUEL RAMIREZ/Jalasoft.DevLevel1.BookRequestSystem/Genre.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jalasoft.DevLevel1.BookRequestSystem
+{
+    public enum Genre
+    {
+        Fiction,
+        NonFiction,
+        Poetry,
+        Romance,
+        ScienceFiction,
+        Fantasy,
+        Mystery,
+        Thriller,
+        Horror,
+        Childrens
+    }
+}
