250c7ad88f23688979a8325fb8fb785e0116b07a
Ivan Fernandez <ivan.fernandez@jala.university>
2023-01-02 16:48:30 -0400

    improvements added

--- PRACTICES/PRACTICE 3/IVAN FERNANDEZ/Jalasoft.Devlevel1.LibraryApp/Jalasoft.Devlevel1.LibraryApp/Novel.cs
+++ PRACTICES/PRACTICE 3/IVAN FERNANDEZ/Jalasoft.Devlevel1.LibraryApp/Jalasoft.Devlevel1.LibraryApp/Novel.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Jalasoft.Devlevel1.BookRequestSystem
 {
-    internal class Novel
+    internal class Novel : Book
     {
+        public Novel(string author, string name, string genre, string languaje, double priceOfPurchase, string iSBNNumber) : base(author, name, genre, languaje, priceOfPurchase, iSBNNumber)
+        {
+        }
+
+        public override void AddBookInfo()
+        {
+            Console.WriteLine($"The Novel '{this.LibraryCode} - {this.Name}' was added at the library so now it is not Available");
+        }
+        public override void LendInfo()
+        {
+            Console.WriteLine($"The Novel '{this.LibraryCode} - {this.Name}' was lend to '{this.LenderName}' so now it is not Available");
+        }
+        public override void RemoveInfo()
+        {
+            Console.WriteLine($"The Novel '{this.LibraryCode} - {this.Name}' was removed from de library so now it is not Available");
+        }
+        public override void ReturnInfo()
+        {
+            Console.WriteLine($"The Novel '{this.LibraryCode} - {this.Name}' was returnd to library so it is now Available");
+        }
     }
 }
--- PRACTICES/PRACTICE 3/IVAN FERNANDEZ/Jalasoft.Devlevel1.LibraryApp/Jalasoft.Devlevel1.LibraryApp/Program.cs
+++ PRACTICES/PRACTICE 3/IVAN FERNANDEZ/Jalasoft.Devlevel1.LibraryApp/Jalasoft.Devlevel1.LibraryApp/Program.cs
@@ -1,7 +1,33 @@
-﻿internal class Program
+﻿using Jalasoft.Devlevel1.BookRequestSystem;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        BookManager myBookManager = new BookManager();
+        Book myComic1 = new Comic("Stan lee", "SpiderMan N1", "Action", "english", 1.99, "11111");
+        Book myComic2 = new Comic("Stan lee", "SpiderMan N2", "Action", "english", 1.99, "22222");
+        Book myNovel1 = new Novel("Alcides Arguedas", "Raza de Bronce", "National Romantic", "Spanish", 4.99, "33333");
+        Book myEssay1 = new Essay("Ivan Fernandez", "Good Learning", "educational", "English", 0.00, "44444");
+        
+        myBookManager.AddBook(myComic1);
+        myBookManager.AddBook(myComic2);
+        myBookManager.LendBook(myComic1,"Ivan Fernandez");
+        myBookManager.LendBook(myComic1, "Alejandro Espinoza");
+        myBookManager.ReturnBook(myComic1);
+        myBookManager.RemoveBook(myComic1);
+        myBookManager.LendBook(myComic1, "Gaby");
+        
+        Console.WriteLine("--------------------*--------------------");
+        myBookManager.AddBook(myNovel1);
+        myBookManager.LendBook(myNovel1, "Eduardo Caba");
+        myBookManager.ReturnBook(myNovel1);
+        
+        Console.WriteLine("--------------------*--------------------");
+        myBookManager.AddBook(myEssay1);
+        myBookManager.RemoveBook(myEssay1);
+        myBookManager.LendBook(myEssay1, "Gaby");
+
+
     }
 }
