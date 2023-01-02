e18b6d983e238c66e7c6483955e623659fbde4ab
Henry Barboza <henry.barboza@jala.university>
2023-01-02 16:26:14 -0400

    Adding 3 types of lists in BookManager

--- PRACTICES/PRACTICE 3/HENRY BARBOZA/Jalasoft.DevLevel1.Practice3/Jalasoft.DevLevel1.Practice3/BookManager.cs
+++ PRACTICES/PRACTICE 3/HENRY BARBOZA/Jalasoft.DevLevel1.Practice3/Jalasoft.DevLevel1.Practice3/BookManager.cs
@@ -9,15 +9,38 @@ namespace Jalasoft.DevLevel1.Practice3
     internal class BookManager<TBook>
         where TBook : Book
     {
-        public List<TBook> BookList = new List<TBook>();
+        public List<TBook> ComicList = new List<TBook>();
+        public List<TBook> EssayList = new List<TBook>();
+        public List<TBook> NovelList = new List<TBook>();
 
         public void AddBook(TBook book)
         {
-            BookList.Add(book);
+            Type type = book.GetType();
+            if (type.Equals(typeof(Comic))) {
+                ComicList.Add(book);
+            } else if(type.Equals(typeof(Essay)))
+            {
+                EssayList.Add(book);
+            } else if (type.Equals(typeof(Novel)))
+            {
+                NovelList.Add(book);
+            }
         }
         public void RemoveBook(TBook book)
         {
-            BookList.Remove(book);
+            Type type = book.GetType();
+            if (type.Equals(typeof(Comic)))
+            {
+                ComicList.Remove(book);
+            }
+            else if (type.Equals(typeof(Essay)))
+            {
+                EssayList.Remove(book);
+            }
+            else if (type.Equals(typeof(Novel)))
+            {
+                NovelList.Remove(book);
+            }
         }
         public void LendBook(TBook book, string userName)
         {
@@ -26,7 +49,17 @@ namespace Jalasoft.DevLevel1.Practice3
         public void ListBooks()
         {
             int index = 0;
-            foreach (TBook book in BookList)
+            foreach (TBook book in ComicList)
+            {
+                index++;
+                Console.WriteLine(index + ". Name: " + book.Name + " Status: " + (book.Status ? "Unavailable" + " Lender Name: " + book.LenderName : "Available"));
+            }
+            foreach (TBook book in EssayList)
+            {
+                index++;
+                Console.WriteLine(index + ". Name: " + book.Name + " Status: " + (book.Status ? "Unavailable" + " Lender Name: " + book.LenderName : "Available"));
+            }
+            foreach (TBook book in NovelList)
             {
                 index++;
                 Console.WriteLine(index + ". Name: " + book.Name + " Status: " + (book.Status ? "Unavailable" + " Lender Name: " + book.LenderName : "Available"));
