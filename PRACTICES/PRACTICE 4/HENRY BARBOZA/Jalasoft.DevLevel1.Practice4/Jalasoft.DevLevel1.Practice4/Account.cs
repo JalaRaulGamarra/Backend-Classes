4271155fb2a283b1f5ffb21b0d159d4041e04a7c
Henry Barboza <henry.barboza@jala.university>
2023-01-08 02:15:13 -0400

    Add Practice4

---
+++ PRACTICES/PRACTICE 4/HENRY BARBOZA/Jalasoft.DevLevel1.Practice4/Jalasoft.DevLevel1.Practice4/Account.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jalasoft.DevLevel1.Practice4
+{
+    internal class Account : IAccount
+    {
+        private int acountBalance = 0;
+        private List<Tuple<string,int>> operations = new List<Tuple<string, int>>();
+
+        public string Deposit(int amount)
+        {
+            CheckValidAmout(amount);
+            acountBalance += amount;
+            operations.Add(new Tuple<string, int>("Deposit", amount));
+            return $"Successful deposit ({amount})";
+        }
+
+        public string Withdraw(int amount)
+        {
+            CheckValidAmout(amount);
+            if (amount > acountBalance)
+            {
+                throw new InsufficientMoneyException("Not enough money in account");
+            }
+            acountBalance -= amount;
+            operations.Add(new Tuple<string, int>("Withdraw", amount));
+            return $"Successful withdrawal ({amount})";
+        }
+
+        public string ShowOperations(int value)
+        {
+            string message = "";
+            var query = operations.Where(operation => operation.Item2 >= 100);
+            foreach (Tuple<string, int> operation in query)
+            {
+                message += $"{operation.Item1}: {operation.Item2} \n";
+            }
+            return message;
+        }
+
+        public string ShowOperations()
+        {
+            string message = ""; 
+            foreach (Tuple<string, int> operation in operations)
+            {
+                message += $"{operation.Item1}: {operation.Item2} \n";
+            }
+            return message;
+        }
+
+        public string ShowBalance()
+        {
+            return $"Account balance: {acountBalance}";
+        }
+
+        private void CheckValidAmout(int amount)
+        {
+            if (amount % 10 != 0)
+            {
+                throw new DepositNotMultipleOfTenException("The account can only receive multiples of 10");
+            }
+        }
+
+    }
+}
---
+++ PRACTICES/PRACTICE 4/HENRY BARBOZA/Jalasoft.DevLevel1.Practice4/Jalasoft.DevLevel1.Practice4/DepositNotMultipleOfTenException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jalasoft.DevLevel1.Practice4
+{
+    internal class DepositNotMultipleOfTenException : Exception
+    {
+        public DepositNotMultipleOfTenException()
+        {
+        }
+
+        public DepositNotMultipleOfTenException(string message)
+            : base(message)
+        {
+        }
+
+        public DepositNotMultipleOfTenException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
