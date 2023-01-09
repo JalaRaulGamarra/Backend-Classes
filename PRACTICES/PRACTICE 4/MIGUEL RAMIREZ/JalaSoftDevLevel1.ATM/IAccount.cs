d6c87748d33eadf68d71c300036513d85ae48be1
Miguel Ramirez <Miguel.Ramirez@jala.university>
2023-01-09 00:10:42 -0400

    Add practice 4

---
+++ PRACTICES/PRACTICE 4/MIGUEL RAMIREZ/JalaSoftDevLevel1.ATM/IAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JalaSoftDevLevel1.ATM
+{
+    public interface IAccount
+    {
+        void Deposit(Transaction transaction);
+        void WithDraw(Transaction transaction);
+        void ShowOperations(int? amount = null, OperationType? type = null, DateTime? startDate = null, DateTime? endDate = null);
+        void ShowOperations();
+        void ShowBalance();
+    }
+}
---
+++ PRACTICES/PRACTICE 4/MIGUEL RAMIREZ/JalaSoftDevLevel1.ATM/InvalidDepositException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JalaSoftDevLevel1.ATM
+{
+    public class InvalidDepositException : Exception
+    {
+        public InvalidDepositException(): base("The account can only receive multiples of 10")
+        {
+        }
+    }
+}
