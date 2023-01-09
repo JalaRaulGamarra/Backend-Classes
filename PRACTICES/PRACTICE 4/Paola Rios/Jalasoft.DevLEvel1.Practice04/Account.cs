f19bbf6f689f5ebd80bcc509c06da3ca05fb9376
Paola Rios <Paola.Rios@jala.university>
2023-01-08 22:16:04 -0400

    Add Practice04 ATM

---
+++ PRACTICES/PRACTICE 4/Paola Rios/Jalasoft.DevLEvel1.Practice04/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jalasoft.DevLEvel1.Practice04
+{
+    public class Account : IAccount
+    {
+        private double accountBalance = 0;
+        private List<Operation> Operations = new List<Operation>();
+
+        public void Deposit(int amount)
+        {
+            if (amount % 10 != 0) 
+            {
+                throw new NotMultipleOfTenException();
+            }
+            accountBalance += amount;
+            Operations.Add(new Operation(){InitialBalance=accountBalance,EndBalance= accountBalance, Amount=amount,TransactionType= OperationType.Deposit });
+
+        }
+        public void ShowBalance() => Console.WriteLine("The account balance is:{0}", accountBalance);
+
+        public void ShowOperation(double mount)
+        {
+            var query = Operations.Where(item => item.Amount >= mount);
+            foreach (var x in query)
+            {
+                Console.WriteLine("The transactions is greater than {0}, is the following deposit :{1}", mount, x.Amount);
+            }
+        }
+        public void ShowOperation()
+        {
+            Operations.ForEach(x => x.Show());
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount > accountBalance)
+            {
+                throw new NotEnoughMoneyException();
+            }
+           accountBalance -= amount;
+           Operations.Add(new Operation() { InitialBalance = accountBalance, EndBalance = accountBalance, Amount = amount, TransactionType = OperationType.WithDraw});
+
+        }   
+    }
+}
---
+++ PRACTICES/PRACTICE 4/Paola Rios/Jalasoft.DevLEvel1.Practice04/Operation.cs
@@ -0,0 +1,15 @@
+﻿namespace Jalasoft.DevLEvel1.Practice04
+{
+    public class Operation
+    {
+        public double InitialBalance { get; set; }
+        public double EndBalance { get; set; }
+        public double Amount { get; set; }
+        public OperationType TransactionType { get; set; }
+
+        internal void Show()
+        {
+            Console.WriteLine("The {0} is :{1}", TransactionType, Amount);
+        }
+    }
+}
