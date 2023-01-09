ae5978f7dd4951d758b78cbdc2237c0090085773
Lina Ramirez <Lina.Ramirez@jala.university>
2023-01-09 15:34:45 -0400

    Add task

---
+++ PRACTICES/PRACTICE 4/Lina Ramirez/JalaSoft.Dev.Level1.SystemATM/JalaSoft.Dev.Level1.SystemATM/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JalaSoft.Dev.Level1.SystemATM
+{
+    internal class Account: IAccount
+    {
+        private double accountBalance;
+        public List<Operations> operations;
+
+        public Account()
+        {
+            accountBalance = 0;
+            operations = new List<Operations>();
+        }
+
+        public void Deposit(double amountMoney)
+        {
+            accountBalance = accountBalance + amountMoney;
+            Operations operation = new Operations(TypeOfTransaction.Deposit, amountMoney);
+            operations.Add(operation);
+        }
+
+        public void ShowBalance()
+        {
+            Console.WriteLine($"The account balance is: {accountBalance}");
+        }
+
+        public void ShowOperations(double amountMoney)
+        {
+            List<Operations> query = operations.Where(e => e.AmountMoney >= amountMoney).ToList();
+            foreach (Operations item in query)
+            {
+                Console.WriteLine($"Type of operation {item.Type} with value {item.AmountMoney}");
+            }
+        }
+
+        public void Withdraw(double amountMoney)
+        {
+            accountBalance = accountBalance - amountMoney;
+            Operations operation = new Operations(TypeOfTransaction.Whithdraw, amountMoney);
+            operations.Add(operation);
+        }
+
+        
+    }
+}
---
+++ PRACTICES/PRACTICE 4/Lina Ramirez/JalaSoft.Dev.Level1.SystemATM/JalaSoft.Dev.Level1.SystemATM/CustomException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JalaSoft.Dev.Level1.SystemATM
+{
+    internal class CustomException: Exception
+    {
+        internal class NoFoundException : Exception
+        {
+            public NoFoundException() : base("Not enough money in account ")
+            {
+            }
+        }
+
+        internal class NotMultipleOfTenException : Exception
+        {
+            public NotMultipleOfTenException() : base("The account can only receive multiples of 10")
+            {
+            }
+        }
+    }
+}
